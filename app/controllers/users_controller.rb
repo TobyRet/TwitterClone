cd073efac81944510d515d8b3caa1b0e737d027e
Toby Retallick <toby.retallick@gmail.com>
2015-01-10 23:36:58 +0000

    Form displays error message if incorrect information provided by user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,17 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end
 
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		# do shit
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+	  def user_params
+	  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	  end
 end
